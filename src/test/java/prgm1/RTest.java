e91c7f5af90170af74282cce7e9066049db527af
GAGAN <gag903536@gmail.com>
2026-04-26 10:56:54 +0530

    success

---
+++ src/test/java/prgm1/RTest.java
@@ -0,0 +1,17 @@
+package prgm1; 
+import org.testng.Assert;
+import org.testng.annotations.Test; 
+public class RTest { 
+ @Test 
+ public void testPass() { 
+ Assert.assertEquals(Result.display(55), "pass"); 
+ } 
+ @Test 
+ public void testFail() { 
+ Assert.assertEquals(Result.display(35), "fail"); 
+ } 
+ @Test 
+ public void testInvalid() { 
+ Assert.assertEquals(Result.display(-5), "invalid"); 
+ } 
+} 
